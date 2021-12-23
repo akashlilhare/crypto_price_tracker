ba02a94775a0a800ec394ee32f05efae9e4f2964
Akash Lilhare <akashlilhare14@gmail.com>
2021-12-24 01:54:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/crypro_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crypro_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+import 'coin_card.dart';
+import 'coin_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      statusBarColor: Colors.transparent,
+    ));
+    return  MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme:ThemeData.dark() ,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      print(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+        setState(() {
+          coinList;
+        });
+      }
+      return coinList;
+    } else {
+      throw Exception('Failed to load coins');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchCoin();
+    Timer.periodic(Duration(seconds: 10), (timer) => fetchCoin());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    //    backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+     //     backgroundColor: Colors.grey[300],
+          title: Text(
+            'CRYPTO TRACKER',
+            style: TextStyle(
+      //        color: Colors.grey[900],
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: coinList.length,
+          itemBuilder: (context, index) {
+            return CoinCard(
+              name: coinList[index].name,
+              symbol: coinList[index].symbol,
+              imageUrl: coinList[index].imageUrl,
+              price: coinList[index].price.toDouble(),
+              change: coinList[index].change.toDouble(),
+              changePercentage: coinList[index].changePercentage.toDouble(),
+            );
+          },
+        ));
+  }
+}
